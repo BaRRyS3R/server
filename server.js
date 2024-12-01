78df6076c60fa29c13a113ad807c1c8e969046c0
BaRRyS3R <mr.crowley.audio@gmail.com>
2024-12-01 17:18:32 +0300

    go

--- server.js
+++ server.js
@@ -1,51 +1,65 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
-const socket_io_1 = require("socket.io");
-// Создаем приложение Express
-const app = (0, express_1.default)();
-const server = http_1.default.createServer(app);
+// server.js
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+// Создаём приложение Express
+const app = express();
+const server = http.createServer(app);
+
 // Инициализируем Socket.IO сервер
-const io = new socket_io_1.Server(server, {
-    cors: {
-        origin: "*", // Настройте для продакшена
-        methods: ["GET", "POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Замените "*" на ваш домен в продакшене для безопасности
+    methods: ["GET", "POST"]
+  }
 });
-// Хранилище подключенных игроков
+
+// Хранилище подключённых игроков
 let players = {};
+
+// Обработка подключений
 io.on('connection', (socket) => {
-    console.log(`Пользователь подключился: ${socket.id}`);
-    // Обработка события ping от клиента
-    socket.on('ping', () => {
-        socket.emit('pong');
-    });
-    // Когда новый игрок присоединяется
-    socket.on('newPlayer', (playerInfo) => {
-        players[socket.id] = playerInfo;
-        io.emit('updatePlayers', players);
-    });
-    // Когда игрок двигается
-    socket.on('playerMovement', (movementData) => {
-        if (players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            io.emit('updatePlayers', players);
-        }
-    });
-    // Когда игрок отключается
-    socket.on('disconnect', () => {
-        console.log(`Пользователь отключился: ${socket.id}`);
-        delete players[socket.id];
-        io.emit('updatePlayers', players);
-    });
+  console.log(`Пользователь подключился: ${socket.id}`);
+
+  // Обработка события ping от клиента
+  socket.on('ping', () => {
+    socket.emit('pong');
+  });
+
+  // Когда новый игрок присоединяется
+  socket.on('newPlayer', (playerInfo) => {
+    players[socket.id] = playerInfo;
+    io.emit('updatePlayers', players);
+  });
+
+  // Когда игрок двигается
+  socket.on('playerMovement', (movementData) => {
+    if (players[socket.id]) {
+      players[socket.id].x = movementData.x;
+      players[socket.id].y = movementData.y;
+      io.emit('updatePlayers', players);
+    }
+  });
+
+  // Когда игрок отключается
+  socket.on('disconnect', () => {
+    console.log(`Пользователь отключился: ${socket.id}`);
+    delete players[socket.id];
+    io.emit('updatePlayers', players);
+  });
 });
-// Запуск сервера
+
+// Маршрут для проверки работоспособности сервера
+app.get('/', (req, res) => {
+  res.send('Socket.IO сервер работает!');
+});
+
+// Определяем порт из переменных окружения или по умолчанию
 const PORT = process.env.PORT || 3000;
+
+// Запуск сервера
 server.listen(PORT, () => {
-    console.log(`Сервер слушает на порту ${PORT}`);
+  console.log(`Сервер слушает на порту ${PORT}`);
 });
